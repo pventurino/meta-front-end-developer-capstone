8daf3009d6920f1b9593afcdd4cd8fd5ca67f798
Pablo Venturino <pablojventurino@gmail.com>
2023-02-06 21:58:09 -0800

    add page structure

--- src/App.js
+++ src/App.js
@@ -5,9 +5,27 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Homepage
-      </div>
+      <>
+        <header>
+          <img src={logo} alt="Little Lemon logo"></img>
+          <nav>
+            <ul>
+              <li><a href="#home">Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#menu">Menu</a></li>
+              <li><a href="#reservations">Reservations</a></li>
+              <li><a href="#orderonline">Order Online</a></li>
+              <li><a href="#login">Login</a></li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          <section id="highlights"></section>
+          <section id="testimonies"></section>
+          <section id="about"></section>
+        </main>
+        <footer></footer>
+      </>
     );
   }
 }
