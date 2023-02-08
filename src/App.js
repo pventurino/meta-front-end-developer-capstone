ceaa9ed90a5d0484cbf15c388b73cc96fbec6a73
Pablo Venturino <pablojventurino@gmail.com>
2023-02-08 12:02:22 -0800

    add css structure

--- src/App.js
+++ src/App.js
@@ -1,30 +1,54 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Container from './components/Container';
 
 class App extends Component {
   render() {
     return (
       <>
         <header>
-          <img src={logo} alt="Little Lemon logo"></img>
-          <nav>
-            <ul>
-              <li><a href="#home">Home</a></li>
-              <li><a href="#about">About</a></li>
-              <li><a href="#menu">Menu</a></li>
-              <li><a href="#reservations">Reservations</a></li>
-              <li><a href="#orderonline">Order Online</a></li>
-              <li><a href="#login">Login</a></li>
-            </ul>
-          </nav>
+          <Container>
+            <img src={logo} alt="Little Lemon logo"></img>
+            <nav>
+              <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#menu">Menu</a></li>
+                <li><a href="#reservations">Reservations</a></li>
+                <li><a href="#orderonline">Order Online</a></li>
+                <li><a href="#login">Login</a></li>
+              </ul>
+            </nav>
+          </Container>
         </header>
         <main>
-          <section id="highlights"></section>
-          <section id="testimonies"></section>
-          <section id="about"></section>
+          <section id="highlights">
+            <Container>
+              <h1>highlights</h1>
+            </Container>
+          </section>
+          <section id="specials">
+            <Container>
+              <h1>specials</h1>
+            </Container>
+          </section>
+          <section id="testimonies">
+            <Container>
+              <h1>testimonies</h1>
+            </Container>
+          </section>
+          <section id="about">
+            <Container>
+              <h1>about</h1>
+            </Container>
+          </section>
         </main>
-        <footer></footer>
+        <footer>
+          <Container>
+            footer
+          </Container>
+        </footer>
       </>
     );
   }
---
+++ src/components/Container.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default props => (
+  <div className="container">
+    {props.children}
+  </div>
+)
