860731507f2cd419b4144487496d4bd988c1eb14
Pablo Venturino <pablojventurino@gmail.com>
2023-02-08 15:32:34 -0800

    add header component & install sass

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Container from './components/Container';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
       <>
         <header>
-          <Container>
-            <img src={logo} alt="Little Lemon logo"></img>
-            <nav>
-              <ul>
-                <li><a href="#home">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#menu">Menu</a></li>
-                <li><a href="#reservations">Reservations</a></li>
-                <li><a href="#orderonline">Order Online</a></li>
-                <li><a href="#login">Login</a></li>
-              </ul>
-            </nav>
-          </Container>
+          <Header/>
         </header>
         <main>
           <section id="highlights">
---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Container from "./Container";
+
+import logo from '../logo.svg';
+
+const Header = props => (
+  <Container>
+    <img src={logo} alt="Little Lemon logo"></img>
+    <nav>
+      <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#menu">Menu</a></li>
+        <li><a href="#reservations">Reservations</a></li>
+        <li><a href="#orderonline">Order Online</a></li>
+        <li><a href="#login">Login</a></li>
+      </ul>
+    </nav>
+  </Container>
+)
+
+export default Header;
