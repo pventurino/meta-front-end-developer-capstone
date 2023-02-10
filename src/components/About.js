dd0abf1a967de8613c8ad41a3d95cedea6cce241
Pablo Venturino <pablojventurino@gmail.com>
2023-02-09 18:03:05 -0800

    add About section

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,28 @@
 import Container from './Container';
+import './About.scss';
+import photo1 from '../img/Mario and Adrian A.jpg';
+import photo2 from '../img/Mario and Adrian B.jpg';
 
 const About = props => (
   <Container>
-    <h1>About</h1>
+    <div className='text'>
+      <h1>Little Lemon</h1>
+      <h2>Chicago</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna
+        aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+        ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit in voluptate velit
+        esse cillum dolore eu fugiat nulla pariatur.
+        Excepteur sint occaecat cupidatat non proident, sunt in
+        culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+    </div>
+    <div className='images'>
+      <img className='photo1' src={photo1} alt='Mario and Adrian'/>
+      <img className='photo2' src={photo2} alt='Mario and Adrian again'/>
+    </div>
   </Container>
 )
 
