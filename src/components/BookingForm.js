297cfae4089d53d635920f6485eccfdbfce1fc36
Pablo Venturino <pablojventurino@gmail.com>
2023-02-17 13:23:37 -0800

    useFormik instead of Formik context

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Formik, Form, Field, ErrorMessage} from 'formik';
+import React from 'react';
+import { useFormik } from 'formik';
 import * as Yup from 'yup';
 
 const BookingForm = ({availableTimes, onDateChange}) => {
@@ -8,63 +8,97 @@ const BookingForm = ({availableTimes, onDateChange}) => {
   let offsetInMillis = 60 * 1000 * utcDate.getTimezoneOffset();
   let localDate = new Date(utcDate - offsetInMillis);
 
-  let [state, setState] = useState({
-    'res-date': localDate.toISOString().substring(0,10),
-    'res-time': 17,
-    'guests': 2,
-    'occasion': 'Birthday'
+  const doSubmit = (...args) => console.log(...args);
+
+  const formik = useFormik({
+    onSubmit: values => doSubmit(values),
+    initialValues: {
+      'res-date': localDate.toISOString().substring(0,10),
+      'res-time': 17,
+      'guests': 2,
+      'occasion': 'Birthday'
+    },
+    validationSchema: Yup.object({
+      'res-date': Yup.date().required(),
+      'res-time': Yup.string().oneOf(availableTimes).required(),
+      'guests': Yup.number().positive().min(1).max(10).required(),
+      'occasion': Yup.string().oneOf(['Birthday','Anniversary'])
+    }),
   });
 
+  const ErrorMessage = (props) => !!formik.errors[props.name] ? (
+    <label
+      className='errorLabel'
+      style={{color: 'red'}}
+      htmlFor={props.name}>
+      {formik.errors[props.name]}
+    </label>
+  ) : null;
+  
   return (
-    <Formik
-      initialValues={state}
-      validationSchema={ Yup.object().shape({
-        'res-date': Yup.date().required(),
-        'res-time': Yup.number().oneOf(availableTimes).required(),
-        'guests': Yup.number().positive().min(1).max(10).required(),
-        'occasion': Yup.string().oneOf(['Birthday','Anniversary'])
-      }) }
-      onSubmit={ values => {
-        console.log(values);
-        setState(values);
-      } }
-    >
-      {({errors, touched}) => (
-        <Form style={{display:'grid', maxWidth:'200px', gap:'20px'}}>
-          <div className='formField'>
-            <label htmlFor='res-date'>Choose date</label>
-            <Field id='res-date' name='res-date' type='date' onChange={(e) => onDateChange(e.currentTarget.value)}/>
-            <ErrorMessage name='res-date' />
-          </div>
+    <form
+      style={{display:'grid', maxWidth:'200px', gap:'20px'}}
+      onSubmit={formik.handleSubmit}>
+      <div className='formField'>
+        <label htmlFor='res-date'>Choose date</label>
+        <input
+          id='res-date'
+          name='res-date'
+          type='date'
+          value={formik.values['res-date']}
+          onBlur={formik.onBlur}
+          onChange={formik.handleChange}
+          />
+        <ErrorMessage name='res-date' />
+      </div>
 
-          <div className='formField'>
-            <label htmlFor='res-time'>Choose time</label>
-            <Field id='res-time' name='res-time' as='select'>
-              { availableTimes.map(x => (<option key={x} value={x}>{x}:00</option>)) }
-            </Field>
-            <ErrorMessage name='res-time' />
-          </div>
+      <div className='formField'>
+        <label htmlFor='res-time'>Choose time</label>
+        <select
+          id='res-time'
+          name='res-time'
+          value={formik.values['res-time']}
+          onBlur={formik.onBlur}
+          onChange={formik.handleChange}
+          >
+          { availableTimes.map(x => (<option key={x} value={x}>{x}</option>)) }
+        </select>
+        <ErrorMessage name='res-time' />
+      </div>
 
-          <div className='formField'>
-            <label htmlFor='guests'>Number of guests</label>
-            <Field id='guests' name='guests' type='number' min={1} max={10}/>
-            <ErrorMessage name='guests'/>
-          </div>
+      <div className='formField'>
+        <label htmlFor='guests'>Number of guests</label>
+        <input
+          id='guests'
+          name='guests'
+          type='number'
+          min={1}
+          max={10}
+          value={formik.values['guests']}
+          onBlur={formik.onBlur}
+          onChange={formik.handleChange}
+          />
+        <ErrorMessage name='guests'/>
+      </div>
 
-          <div className='formField'>
-            <label htmlFor='occasion'>Occasion</label>
-            <Field id='occasion' name='occasion' as='select'>
-              <option key='birthday' value='Birthday'>Birthday</option>
-              <option key='anniversary' value='Anniversary'>Anniversary</option>
-            </Field>
-            <ErrorMessage name='occasion'/>
-          </div>
+      <div className='formField'>
+        <label htmlFor='occasion'>Occasion</label>
+        <select
+          id='occasion'
+          name='occasion'
+          as='select'
+          value={formik.values['occasion']}
+          onBlur={formik.onBlur}
+          onChange={formik.handleChange}
+          >
+          <option key='birthday' value='Birthday'>Birthday</option>
+          <option key='anniversary' value='Anniversary'>Anniversary</option>
+        </select>
+        <ErrorMessage name='occasion'/>
+      </div>
 
-          <button type='submit'>Make Your reservation</button>
-        </Form>
-      )}
-    </Formik>
-
+      <button type='submit' role='submit'>Make Your reservation</button>
+    </form>
 )
 };
 
